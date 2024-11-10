f374ebed27a8e861db709ad9bf54253591098d28
akashe <infiniteakashe@gmail.com>
2024-11-10 21:46:25 +0530

    updating index.html in nginx conf

--- terraform/main.tf
+++ terraform/main.tf
@@ -333,8 +333,8 @@ resource "aws_ecs_task_definition" "app" {
   family                   = "${var.project_name}-task"
   network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
-  cpu                      = 256
-  memory                   = 512
+  cpu                      = 2048
+  memory                   = 4096
   execution_role_arn       = aws_iam_role.ecs_execution_role.arn
   task_role_arn           = aws_iam_role.ecs_task_role.arn
 
